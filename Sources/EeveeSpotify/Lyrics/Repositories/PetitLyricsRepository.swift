54919032af6c7316c4aff49505e14beff7a87041
Luca LeBlanc <yodaluca@hotmail.com>
2024-07-15 19:23:39 -0500

    Use App API

--- Sources/EeveeSpotify/Lyrics/Repositories/PetitLyricsRepository.swift
+++ Sources/EeveeSpotify/Lyrics/Repositories/PetitLyricsRepository.swift
@@ -1,28 +1,28 @@
 import Foundation
 
 struct PetitLyricsRepository: LyricsRepository {
-    private let searchUrl = "https://petitlyrics.com/search_lyrics"
-    private let csrfUrl = "https://petitlyrics.com/lib/pl-lib.js"
-    private let lyricsUrl = "https://petitlyrics.com/com/get_lyrics.ajax"
+    private let apiUrl = "https://p1.petitlyrics.com/api/GetPetitLyricsData.php"
     private let session: URLSession
 
     init() {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = [
+            "Content-Type": "application/x-www-form-urlencoded",
             "User-Agent": "EeveeSpotify v\(EeveeSpotify.version) https://github.com/whoeevee/EeveeSpotify"
         ]
+        
         session = URLSession(configuration: configuration)
     }
-
-    private func perform(_ url: String, method: String = "GET", headers: [String: String] = [:], body: [String: String] = [:]) throws -> Data {
-        var request = URLRequest(url: URL(string: url)!)
-        request.httpMethod = method
-        request.allHTTPHeaderFields = headers
-
-        if method == "POST" {
-            request.httpBody = body.queryString.data(using: .utf8)
-        }
-
+    
+    private func perform(
+        _ query: [String: Any]
+    ) throws -> Data {
+        var request = URLRequest(url: URL(string: apiUrl)!)
+        request.httpMethod = "POST"
+        
+        let queryString = query.queryString
+        request.httpBody = queryString.data(using: .utf8)
+        
         let semaphore = DispatchSemaphore(value: 0)
         var data: Data?
         var error: Error?
@@ -42,63 +42,145 @@ struct PetitLyricsRepository: LyricsRepository {
 
         return data!
     }
-
-    private func extractLyricId(from html: String) -> String? {
-        let regex = try! NSRegularExpression(pattern: "/lyrics/(\\d+)")
-        let range = NSRange(location: 0, length: html.utf16.count)
-        if let match = regex.firstMatch(in: html, options: [], range: range) {
-            if let range = Range(match.range(at: 1), in: html) {
-                return String(html[range])
-            }
+    
+    private func parseXML(_ data: Data) throws -> [String: Any] {
+        let parser = XMLParser(data: data)
+        let delegate = XMLParserDelegateImpl()
+        parser.delegate = delegate
+        
+        if parser.parse() {
+            return delegate.result
+        } else {
+            throw LyricsError.DecodingError
         }
-        return nil
     }
-
-    private func extractCsrfToken(from js: String) -> String? {
-        let regex = try! NSRegularExpression(pattern: "X-CSRF-Token',\\s*'([^']+)'")
-        let range = NSRange(location: 0, length: js.utf16.count)
-        if let match = regex.firstMatch(in: js, options: [], range: range) {
-            if let range = Range(match.range(at: 1), in: js) {
-                return String(js[range])
-            }
+    
+    private func decodeBase64(_ base64String: String) throws -> Data {
+        guard let data = Data(base64Encoded: base64String) else {
+            throw LyricsError.DecodingError
         }
-        return nil
+        return data
     }
-
+    
+    private func mapTimeSyncedLyrics(_ xmlData: Data) throws -> [LyricsLineDto] {
+        let parser = XMLParser(data: xmlData)
+        let delegate = TimeSyncedLyricsParserDelegate()
+        parser.delegate = delegate
+        
+        if parser.parse() {
+            return delegate.lines
+        } else {
+            throw LyricsError.DecodingError
+        }
+    }
+    
     func getLyrics(_ query: LyricsSearchQuery, options: LyricsOptions) throws -> LyricsDto {
-        let searchData = ["title": query.title, "artist": query.primaryArtist]
-        let searchResponse = try perform(searchUrl, method: "POST", headers: ["Content-Type": "application/x-www-form-urlencoded"], body: searchData)
-        guard let lyricId = extractLyricId(from: String(data: searchResponse, encoding: .utf8)!) else {
+        var petitLyricsQuery = [
+            "maxCount": "1",
+            "key_title": query.title,
+            "key_artist": query.primaryArtist,
+            "terminalType": "10",
+            "clientAppId": "p1232089",
+            "lyricsType": "3"
+        ]
+        
+        let data = try perform(petitLyricsQuery)
+        let xmlResponse = try parseXML(data)
+        
+        guard let returnedCount = xmlResponse["returnedCount"] as? Int, returnedCount > 0 else {
             throw LyricsError.NoSuchSong
         }
-
-        let initialUrl = "https://petitlyrics.com/lyrics/\(lyricId)"
-        _ = try perform(initialUrl)
-
-        let csrfResponse = try perform(csrfUrl)
-        guard let csrfToken = extractCsrfToken(from: String(data: csrfResponse, encoding: .utf8)!) else {
+        
+        guard let song = xmlResponse["song"] as? [String: Any],
+              let lyricsDataBase64 = song["lyricsData"] as? String,
+              let availableLyricsType = song["availableLyricsType"] as? Int else {
             throw LyricsError.DecodingError
         }
-
-        let cookies = HTTPCookieStorage.shared.cookies?.first(where: { $0.name == "PLSESSION" })
-        guard let plsessionCookie = cookies?.value else {
-            throw LyricsError.DecodingError
+        
+        let lyricsData = try decodeBase64(lyricsDataBase64)
+        
+        if availableLyricsType == 2 {
+            petitLyricsQuery["lyricsType"] = "1"
+            let data = try perform(petitLyricsQuery)
+            let xmlResponse = try parseXML(data)
+            
+            guard let song = xmlResponse["song"] as? [String: Any],
+                  let lyricsDataBase64 = song["lyricsData"] as? String else {
+                throw LyricsError.DecodingError
+            }
+            
+            let lyricsData = try decodeBase64(lyricsDataBase64)
+            let lines = String(data: lyricsData, encoding: .utf8)?.components(separatedBy: "\n").map { LyricsLineDto(content: $0) } ?? []
+            
+            return LyricsDto(lines: lines, timeSynced: false)
         }
-
-        let lyricsData = ["lyrics_id": lyricId]
-        let lyricsResponse = try perform(lyricsUrl, method: "POST", headers: [
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Cookie": "PLSESSION=\(plsessionCookie)",
-            "X-CSRF-Token": csrfToken,
-            "X-Requested-With": "XMLHttpRequest"
-        ], body: lyricsData)
-
-        let lyricsJson = try JSONSerialization.jsonObject(with: lyricsResponse, options: []) as! [[String: Any]]
-        let lyricsLines = lyricsJson.map { item -> LyricsLineDto in
-            let decodedLyrics = String(data: Data(base64Encoded: item["lyrics"] as! String)!, encoding: .utf8)!
-            return LyricsLineDto(content: decodedLyrics)
+        
+        if availableLyricsType == 1 {
+            let lines = String(data: lyricsData, encoding: .utf8)?.components(separatedBy: "\n").map { LyricsLineDto(content: $0) } ?? []
+            return LyricsDto(lines: lines, timeSynced: false)
         }
+        
+        if availableLyricsType == 3 {
+            let lines = try mapTimeSyncedLyrics(lyricsData)
+            return LyricsDto(lines: lines, timeSynced: true)
+        }
+        
+        throw LyricsError.DecodingError
+    }
+}
 
-        return LyricsDto(lines: lyricsLines, timeSynced: false)
+private class XMLParserDelegateImpl: NSObject, XMLParserDelegate {
+    var result: [String: Any] = [:]
+    private var currentElement: String = ""
+    private var currentSong: [String: Any] = [:]
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if currentElement == "returnedCount" || currentElement == "availableLyricsType" {
+            result[currentElement] = Int(string)
+        } else if currentElement == "lyricsData" {
+            currentSong[currentElement] = string
+        } else {
+            currentSong[currentElement] = string
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "song" {
+            result["song"] = currentSong
+            currentSong = [:]
+        }
+    }
+}
+
+private class TimeSyncedLyricsParserDelegate: NSObject, XMLParserDelegate {
+    var lines: [LyricsLineDto] = []
+    private var currentElement: String = ""
+    private var currentLine: LyricsLineDto?
+    private var currentStartTime: Int?
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if currentElement == "linestring" {
+            currentLine = LyricsLineDto(content: string)
+        } else if currentElement == "starttime" {
+            currentStartTime = Int(string)
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "line" {
+            if let line = currentLine, let startTime = currentStartTime {
+                lines.append(LyricsLineDto(content: line.content, offsetMs: startTime))
+            }
+            currentLine = nil
+            currentStartTime = nil
+        }
     }
 }
